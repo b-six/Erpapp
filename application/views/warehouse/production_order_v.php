308b642e032edf1a1b98218103a44749997c0efc
Daffa Alberta Votary <daffavotary@gmail.com>
2019-07-15 12:55:36 +0700

    a

--- application/views/warehouse/production_order_v.php
+++ application/views/warehouse/production_order_v.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Production Order</title>
-	<?php $this->load->view('warehouse/_partials/css.php') ?>
+    <title>Marketing - Sales Order</title>
+    <?php $this->load->view('warehouse/_partials/css.php') ?>
 </head>
+
 <body>
-	<?php $this->load->view('warehouse/_partials/navbar.php') ?>
-	
+    <!-- navbar -->
+    <?php $this->load->view('warehouse/_partials/navbar.php') ?>
+
 	<div class="container">
         <br>
         <div class="row">
@@ -25,10 +26,24 @@
             <!-- searchbar -->
             <div class="col s4"><?php $this->load->view('warehouse/_partials/searchbar.php'); ?></div>         
 
-        </div>
 
-        <!-- konten tab -->
-        <div id="tabelproductionorder" class="col s12 white-text content-color">
+         <!-- tab -->
+        <div class="row">
+            <div class="col s12">
+                <ul class="tabs blue-dark-grey">
+                    <li class="tab col s2"><a href="#pending" class="active small-font">Pending</a></li>
+                    <li class="tab col s2"><a class="small-font" href="#on-process">
+                            On-Process</a></li>
+                    <li class="tab col s2"><a href="#success" class="small-font">
+                            Success</a></li>
+                </ul>
+                <br>
+            </div>
+
+            <!-- konten tab -->
+
+            <!-- tab pending -->
+        <div id="pending" class="col s12 white-text content-color">
             <table class="responsive-table centered highlight">
                 <thead class="bottom-border">
                     <tr>
@@ -37,33 +52,34 @@
                         <th>No. Produksi</th>
                         <th>Produk</th>
                         <th>Jumlah</th>
-                        <th>Status</th>
+                        
                     </tr>
                 </thead>
 
                 <tbody>
                 <?php
-                    $count = 0;
+                    //$count = 0;
+                 $hitung = 0;
                     foreach ($production_order->result() as $col) :
-                        $count++;
-                        ?>
-                        <tr>
-                            <?php
-                            //$hitung = 0;
+                        //$count++;
+                         if ($col->status == 'pending') :
+                        
+                            $hitung++;
             
                             foreach ($stock_barang->result() as $row) :
-                              //$hitung++;
+                            
                                 if ($row->id_barang == $col->id_barang) :
                                     ?>
-                                    <td><?php echo $count; ?></td>
+                                    <tr>
+                                    <td><?php echo $hitung; ?></td>
                                     <td><?php echo $col->tanggal; ?></td>
                                     <td><?php echo $col->id_po; ?></td>
                                     <td><?php echo $row->nama_barang; ?></td>
                                     <!--<td><?php echo $row->tanggal_kadaluarsa_barang; ?></td>
                                     <td><?php echo $row->harga_barang; ?></td>-->
                                     <td><?php echo $col->jumlah_pesanan; ?></td>
-                                    <td><?php echo $col->status; ?></td>
-
+                                    
+</tr>
                                 <?php
                             endif;
                         endforeach;
@@ -80,14 +96,137 @@
                                     <a href="#"><i class="material-icons edit-button">create</i></a>
                                 </div>
                             </td>-->
-                        </tr>
+                       
                     <?php
+                endif;
                 endforeach;
                 ?>
                 </tbody>
             </table>
         </div>
 
+
+            <!-- tab onprocess -->
+        <div id="on-process" class="col s12 white-text content-color">
+            <table class="responsive-table centered highlight">
+                <thead class="bottom-border">
+                    <tr>
+                        <th>No</th>
+                        <th>Tanggal</th>
+                        <th>No. Produksi</th>
+                        <th>Produk</th>
+                        <th>Jumlah</th>
+                       
+                    </tr>
+                </thead>
+
+                <tbody>
+                <?php
+                    //$count = 0;
+                 $hitung = 0;
+                    foreach ($production_order->result() as $col) :
+                        //$count++;
+                         if ($col->status == 'onprocess') :
+                         $hitung++;
+                        ?>
+                        
+                            <?php
+                           
+            
+                            foreach ($stock_barang->result() as $row) :
+                            
+                                if ($row->id_barang == $col->id_barang) :
+                                    
+                                    ?>
+                                    <tr>
+                                    <td><?php echo $hitung; ?></td>
+                                    <td><?php echo $col->tanggal; ?></td>
+                                    <td><?php echo $col->id_po; ?></td>
+                                    <td><?php echo $row->nama_barang; ?></td>
+                                    <!--<td><?php echo $row->tanggal_kadaluarsa_barang; ?></td>
+                                    <td><?php echo $row->harga_barang; ?></td>-->
+                                    <td><?php echo $col->jumlah_pesanan; ?></td>
+                                    
+
+                        </tr>
+                                <?php
+                            endif;
+                        endforeach;
+                       
+                endif;
+                endforeach;
+                ?>
+                </tbody>
+            </table>
+        </div>
+
+
+            <!-- tab success -->
+        <div id="success" class="col s12 white-text content-color">
+            <table class="responsive-table centered highlight">
+                <thead class="bottom-border">
+                    <tr>
+                        <th>No</th>
+                        <th>Tanggal</th>
+                        <th>No. Produksi</th>
+                        <th>Produk</th>
+                        <th>Jumlah</th>
+
+                    </tr>
+                </thead>
+
+                <tbody>
+                <?php
+                     $hitung = 0;
+                    foreach ($production_order->result() as $col) :
+                        //$count++;
+                         if ($col->status == 'success') :
+ $hitung++;
+                        ?>
+                       
+                            <?php
+                            ;
+            
+                            foreach ($stock_barang->result() as $row) :
+                           
+                                if ($row->id_barang == $col->id_barang) :
+                                    ?>
+<tr>
+                                    <td><?php echo $hitung; ?></td>
+                                    <td><?php echo $col->tanggal; ?></td>
+                                    <td><?php echo $col->id_po; ?></td>
+                                    <td><?php echo $row->nama_barang; ?></td>
+                                    <!--<td><?php echo $row->tanggal_kadaluarsa_barang; ?></td>
+                                    <td><?php echo $row->harga_barang; ?></td>-->
+                                    <td><?php echo $col->jumlah_pesanan; ?></td>
+                                    
+</tr>
+                                <?php
+                            endif;
+                        endforeach;
+                        ?>
+
+                            <!--
+                            <td><?php echo $col->id_so; ?></td>
+                            <td><?php
+                                $testimoni_barang = substr($col->testimoni_barang, 0, 25);
+                                echo $testimoni_barang; ?></td>
+                            <td class="button-container">
+                                <div id="table-button">
+                                    <a href="#"><i class="material-icons delete-button">delete_forever</i></a> 
+                                    <a href="#"><i class="material-icons edit-button">create</i></a>
+                                </div>
+                            </td>-->
+                       
+                    <?php
+                endif;
+                endforeach;
+                ?>
+                </tbody>
+            </table>
+        </div>
+</div>
+</div>
     </div>
 	
 	<?php $this->load->view('warehouse/_partials/js.php') ?>
--- application/views/warehouse/sales_order_v.php
+++ application/views/warehouse/sales_order_v.php
@@ -1,19 +1,21 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Sales Order</title>
-	<?php $this->load->view('warehouse/_partials/css.php') ?>
+    <title>Marketing - Sales Order</title>
+    <?php $this->load->view('warehouse/_partials/css.php') ?>
 </head>
+
 <body>
-	<?php $this->load->view('warehouse/_partials/navbar.php') ?>
-	
-	<div class="container">
+    <!-- navbar -->
+    <?php $this->load->view('warehouse/_partials/navbar.php') ?>
+
+    <!-- konten -->
+    <div class="container">
         <br>
         <div class="row">
             <!-- breadcrumb -->
-            <div class="col s8">
+            <div class="col s7">
                 <nav class="no-shadows breadcrumbs-style">
                     <div class="nav-wrapper blue-dark-grey">
                         <a class="breadcrumb no-pointer-event">Order</a>
@@ -23,72 +25,175 @@
             </div>
 
             <!-- searchbar -->
-            <div class="col s4"><?php $this->load->view('warehouse/_partials/searchbar.php'); ?></div>         
+            <div class="col s4"><?php $this->load->view('marketing/partials/searchbar.php'); ?></div>
 
-        </div>
+           
 
-        <!-- konten tab -->
-        <div id="tabelsalesorder" class="col s12 white-text content-color">
-            <table class="responsive-table centered highlight">
-                <thead class="bottom-border">
-                    <tr>
-                        <th>No</th>
+        <!-- tab -->
+        <div class="row">
+            <div class="col s12">
+                <ul class="tabs blue-dark-grey">
+                    <li class="tab col s2"><a href="#pending" class="active small-font">Pending</a></li>
+                    <li class="tab col s2"><a class="small-font" href="#on-process">
+                            On-Process</a></li>
+                    <li class="tab col s2"><a href="#success" class="small-font">
+                            Success</a></li>
+                </ul>
+                <br>
+            </div>
+
+            <!-- konten tab -->
+
+            <!-- tab pending -->
+            <div id="pending" class="col s12 white-text content-color">
+                <table class="responsive-table centered highlight">
+                    <thead class="bottom-border">
+                        <tr>
+                            <th>No</th>
                         <th>Tanggal</th>
                         <th>No. Order</th>
                         <th>Customer</th>
                         <th>Total</th>
                         <th>Harga</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                <?php
-                    $count = 0;
-                    foreach ($sales_order->result() as $col) :
-                        $count++;
-                        ?>
-                        <tr>
-                            <?php
-                            //$hitung = 0;
-            
-                            foreach ($customer->result() as $row) :
-                              //$hitung++;
-                                if ($row->id_pelanggan == $col->id_pelanggan) :
-                                    ?>
-                                    <td><?php echo $count; ?></td>
-                                    <td><?php echo $col->tanggal_pesanan; ?></td>
-                                    <td><?php echo $col->id_so; ?></td>
-                                    <td><?php echo $row->nama_pelanggan; ?></td>
-                                    <!--<td><?php echo $row->tanggal_kadaluarsa_barang; ?></td>-->
-                                    <td><?php echo $col->total_barang; ?></td>
-                                    <td><?php echo $col->total_pesanan; ?></td>
- 
-                                <?php
-                            endif;
-                        endforeach;
-                        ?>
-
-                            <!--
-                            <td><?php echo $col->id_so; ?></td>
-                            <td><?php
-                                $testimoni_barang = substr($col->testimoni_barang, 0, 25);
-                                echo $testimoni_barang; ?></td>
-                            <td class="button-container">
-                                <div id="table-button">
-                                    <a href="#"><i class="material-icons delete-button">delete_forever</i></a> 
-                                    <a href="#"><i class="material-icons edit-button">create</i></a>
-                                </div>
-                            </td>-->
                         </tr>
-                    <?php
-                endforeach;
-                ?>
-                </tbody>
-            </table>
-        </div>
+                    </thead>
 
+                    <tbody>
+                        <?php
+                         $hitung = 0;
+                        foreach ($sales_order->result() as $row) :
+                            
+
+                                
+                            if ($row->status == 'pending') :
+$hitung++;
+                                foreach ($customer->result() as $col) :
+                                    
+
+
+                                    if ($col->id_pelanggan == $row->id_pelanggan) :
+                                        $nama_pelanggan = $col->nama_pelanggan; 
+                                       ?>
+                                        <tr>
+                                            <td><?php echo $hitung; ?></td>
+                                    <td><?php echo $row->tanggal_pesanan; ?></td>
+                                    <td><?php echo $row->id_so; ?></td>
+                                    <td><?php echo $col->nama_pelanggan; ?></td>
+                                    <!--<td><?php echo $row->tanggal_kadaluarsa_barang; ?></td>-->
+                                    <td><?php echo $row->total_barang; ?></td>
+                                    <td><?php echo "Rp. " . number_format($row->total_pesanan) ?></td>
+                                            
+                                        </tr>
+                                    <?php endif;
+                            endforeach;
+                        endif;
+                    endforeach; ?>
+                    </tbody>
+
+                </table>
+            </div>
+
+            <!-- tab onprocess -->
+            <div id="on-process" class="col s12 white-text content-color">
+                <table class="responsive-table centered highlight">
+                    <thead class="bottom-border">
+                         <tr>
+                            <th>No</th>
+                        <th>Tanggal</th>
+                        <th>No. Order</th>
+                        <th>Customer</th>
+                        <th>Total</th>
+                        <th>Harga</th>
+                        </tr>
+                    </thead>
+
+                   <tbody>
+                        <?php
+                        $hitung = 0;
+                        foreach ($sales_order->result() as $row) :
+                            
+
+                            if ($row->status == 'onprocess') :
+
+                                $hitung++;
+                                foreach ($customer->result() as $col) :
+                                    
+
+
+                                  if ($col->id_pelanggan == $row->id_pelanggan) :
+                                        $nama_pelanggan = $col->nama_pelanggan; 
+                                        ?>
+                                        <tr>
+                                            <td><?php echo $hitung; ?></td>
+                                    <td><?php echo $row->tanggal_pesanan; ?></td>
+                                    <td><?php echo $row->id_so; ?></td>
+                                    <td><?php echo $col->nama_pelanggan; ?></td>
+                                    <!--<td><?php echo $row->tanggal_kadaluarsa_barang; ?></td>-->
+                                    <td><?php echo $row->total_barang; ?></td>
+                                    <td><?php echo "Rp. " . number_format($row->total_pesanan) ?></td>
+                                            
+                                        </tr>
+                                    <?php endif;
+                            endforeach;
+                        endif;
+                    endforeach; ?>
+                    </tbody>
+
+                </table>
+            </div>
+
+            <!-- tab success -->
+            <div id="success" class="col s12 white-text content-color">
+                <table class="responsive-table centered highlight">
+                    <thead class="bottom-border">
+                         <tr>
+                            <th>No</th>
+                        <th>Tanggal</th>
+                        <th>No. Order</th>
+                        <th>Customer</th>
+                        <th>Total</th>
+                        <th>Harga</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php
+                       $hitung = 0;
+                        foreach ($sales_order->result() as $row) :
+                            
+
+                            if ($row->status == 'success') :
+
+                                 $hitung++;
+                                foreach ($customer->result() as $col) :
+                                   
+
+
+                                    if ($col->id_pelanggan == $row->id_pelanggan) :
+                                        $nama_pelanggan = $col->nama_pelanggan; 
+                                       ?>
+                                        <tr>
+                                            <td><?php echo $hitung; ?></td>
+                                    <td><?php echo $row->tanggal_pesanan; ?></td>
+                                    <td><?php echo $row->id_so; ?></td>
+                                    <td><?php echo $col->nama_pelanggan; ?></td>
+                                    <!--<td><?php echo $row->tanggal_kadaluarsa_barang; ?></td>-->
+                                    <td><?php echo $row->total_barang; ?></td>
+                                    <td><?php echo "Rp. " . number_format($row->total_pesanan) ?></td>
+                                            
+                                        </tr>
+                                    <?php endif;
+                            endforeach;
+                        endif;
+                    endforeach; ?>
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
     </div>
-	
-	<?php $this->load->view('warehouse/_partials/js.php') ?>
+    <!-- js -->
+    <?php $this->load->view('marketing/partials/js.php') ?>
 </body>
+
 </html>
