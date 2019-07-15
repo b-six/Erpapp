f6c9fa625e586b4b5e45f05dcaf75f649c79d1c9
Algorithm <49584601+algorithmsevolve@users.noreply.github.com>
2019-07-15 10:14:58 +0700

    Marketing Update #5
    Update stock_barang table on erpapp.sql database

--- exported-database/erpapp.sql
+++ exported-database/erpapp.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2019 at 11:50 PM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 5.6.36
+-- Waktu pembuatan: 15 Jul 2019 pada 05.13
+-- Versi server: 10.1.32-MariaDB
+-- Versi PHP: 5.6.36
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customer`
+-- Struktur dari tabel `customer`
 --
 
 CREATE TABLE `customer` (
@@ -38,20 +38,20 @@ CREATE TABLE `customer` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `customer`
+-- Dumping data untuk tabel `customer`
 --
 
 INSERT INTO `customer` (`id_pelanggan`, `nama_pelanggan`, `alamat_pelanggan`, `wilayah`, `tipe_customer`, `sejak`) VALUES
-('1125', 'Nasirudin Sabiq', 'Jl. Aowkwkwkwk', 'Depok', 'Personal', '2019-06-05'),
 ('1126', 'RVZ Didan', 'Jl. Medan Raya', 'Medan', 'Retailer', '2019-06-06'),
 ('1127', 'Ryumada Rizuki', 'Jl. Cipayung', 'Jakarta Timur', 'Distributor', '2019-06-07'),
 ('3', 'Akubaruto Rizuki', '', 'Jakarta Selatan', 'Distributor', '2019-06-03'),
-('4', 'Bayu Uicaksono', '', 'Cikupa', 'Personal', '2019-06-03');
+('4', 'Bayu Uicaksono', '', 'Cikupa', 'Personal', '2019-06-03'),
+('5', 'Nasirudin Sabiq', '', 'Depok', 'Personal', '2019-07-09');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `production_order`
+-- Struktur dari tabel `production_order`
 --
 
 CREATE TABLE `production_order` (
@@ -63,18 +63,18 @@ CREATE TABLE `production_order` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `production_order`
+-- Dumping data untuk tabel `production_order`
 --
 
 INSERT INTO `production_order` (`id_po`, `id_barang`, `jumlah_pesanan`, `tanggal`, `status`) VALUES
-('P-201906031', '1', 20, '2019-06-03', 'pending'),
-('P-201906032', '2', 200, '2019-06-03', 'onprocess'),
-('P-201906033', '1', 5555, '2019-06-03', 'success');
+('P-201906033', '1', 5555, '2019-06-03', 'success'),
+('P-201907082', '1', 20, '2019-07-08', 'onprocess'),
+('P-201907083', '1', 40, '2019-07-08', 'pending');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sales_order`
+-- Struktur dari tabel `sales_order`
 --
 
 CREATE TABLE `sales_order` (
@@ -88,20 +88,18 @@ CREATE TABLE `sales_order` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `sales_order`
+-- Dumping data untuk tabel `sales_order`
 --
 
 INSERT INTO `sales_order` (`id_so`, `tanggal_pesanan`, `id_pelanggan`, `total_barang`, `total_pesanan`, `status`, `testimoni`) VALUES
-('201906021', '2019-06-02', '1125', 67, 379000, 'pending', 'N'),
-('201906022', '2019-06-02', '1126', 49, 255000, 'onprocess', 'Y'),
-('201906023', '2019-06-02', '1127', 110, 682000, 'success', 'Y'),
-('201906031', '2019-06-03', '3', 350, 2050000, 'pending', 'Y'),
-('201906032', '2019-06-03', '4', 21, 107000, 'pending', 'N');
+('201906022', '2019-06-02', '4', 2, 12000, 'onprocess', 'N'),
+('201906023', '2019-06-02', '1127', 70, 750000, 'success', 'Y'),
+('201907081', '2019-07-08', '5', 22, 88000, 'pending', 'Y');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `shopping_cart`
+-- Struktur dari tabel `shopping_cart`
 --
 
 CREATE TABLE `shopping_cart` (
@@ -114,47 +112,62 @@ CREATE TABLE `shopping_cart` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `shopping_cart`
+-- Dumping data untuk tabel `shopping_cart`
 --
 
 INSERT INTO `shopping_cart` (`id_sc`, `id_so`, `id_barang`, `tanggal_pesanan`, `jumlah_barang`, `total_harga`) VALUES
-('201906021-1', '201906021', '1', '2019-06-02', 45, 225000),
-('201906021-2', '201906021', '2', '2019-06-02', 22, 154000),
-('201906022-1', '201906022', '1', '2019-06-02', 44, 220000),
-('201906022-2', '201906022', '2', '2019-06-02', 5, 35000),
-('201906023-1', '201906023', '2', '2019-06-02', 66, 462000),
-('201906023-2', '201906023', '1', '2019-06-02', 44, 220000),
-('201906031-1', '201906031', '1', '2019-06-03', 200, 1000000),
-('201906031-2', '201906031', '2', '2019-06-03', 150, 1050000),
-('201906032-1', '201906032', '1', '2019-06-03', 20, 100000),
-('201906032-2', '201906032', '2', '2019-06-03', 1, 7000);
+('201906022-1', '201906022', 'FT-1', '2019-06-02', 2, 12000),
+('201906023-1', '201906023', 'P-3', '2019-06-02', 20, 400000),
+('201906023-2', '201906023', 'TBS-3', '2019-06-02', 50, 350000),
+('201907081-1', '201907081', 'FT-2', '2019-07-08', 2, 8000),
+('201907081-2', '201907081', 'CC-1', '2019-07-08', 20, 80000);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `stock_barang`
+-- Struktur dari tabel `stock_barang`
 --
 
 CREATE TABLE `stock_barang` (
   `id_barang` varchar(30) NOT NULL,
-  `nama_barang` varchar(30) NOT NULL,
+  `nama_barang` varchar(256) NOT NULL,
   `tanggal_kadaluarsa_barang` date NOT NULL,
   `harga_barang` float NOT NULL,
   `jumlah_stok_barang` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `stock_barang`
+-- Dumping data untuk tabel `stock_barang`
 --
 
 INSERT INTO `stock_barang` (`id_barang`, `nama_barang`, `tanggal_kadaluarsa_barang`, `harga_barang`, `jumlah_stok_barang`) VALUES
-('1', 'Teh Kotak', '2020-02-01', 5000, 258),
-('2', 'Teh Botol', '2019-06-30', 7000, 200);
+('CC-1', 'Country Choice (250 mL)', '2020-09-10', 4000, 258),
+('CC-2', 'Country Choice (1 L)', '2019-07-24', 8000, 500),
+('FT-1', 'Fruit Tea Botol Beling (235 mL)', '2020-05-15', 6000, 400),
+('FT-2', 'Fruit Tea Genggam (200 mL)', '2020-03-27', 4000, 500),
+('FT-3', 'Fruit Tea Kaleng (318 mL)', '2020-07-16', 8000, 460),
+('P-1', 'Prim-A Cup (240 mL)', '2020-07-17', 2500, 258),
+('P-2', 'Prim-A Botol (330 mL)', '2020-05-16', 4000, 500),
+('P-3', 'Prim-A Gallon (19 L)', '2020-08-21', 20000, 1500),
+('ST-1', 'S-Tee Botol Beling (318 mL)', '2020-08-21', 4000, 200),
+('ST-2', 'S-Tee Botol Plastik (350 mL)', '2020-10-16', 5000, 500),
+('T-1', 'Tebs Botol Beling (230 mL)', '2020-06-05', 5000, 200),
+('T-2', 'Tebs Botol Plastik (500 mL)', '2020-06-19', 6000, 500),
+('T-3', 'Tebs Kaleng (500 mL)', '2020-11-06', 8000, 300),
+('TBS-1', 'Teh Botol Sosro (220 mL)', '2019-06-30', 7000, 200),
+('TBS-2', 'Teh Kotak Sosro (200mL)', '2020-02-01', 5000, 258),
+('TBS-3', 'Teh Kotak Sosro (250 mL)', '2020-06-19', 7000, 520),
+('TBS-4', 'Teh Kotak Sosro (330 mL)', '2020-05-15', 10000, 500),
+('TBS-5', 'Teh Kotak Sosro (1 L)', '2020-03-13', 11000, 300),
+('TBS-6', 'Teh Botol Sosro Plastik (450 mL)', '2020-09-17', 6000, 460),
+('TBS-7', 'Teh Botol Sosro Plastik (350 mL)', '2020-09-18', 4000, 600),
+('TBS-8', 'Teh Pouch Sosro (230 mL)', '2020-08-07', 5000, 1200),
+('TBS-9', 'Teh Kaleng Sosro (318 mL)', '2020-09-25', 5000, 700);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `testimoni`
+-- Struktur dari tabel `testimoni`
 --
 
 CREATE TABLE `testimoni` (
@@ -165,50 +178,49 @@ CREATE TABLE `testimoni` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `testimoni`
+-- Dumping data untuk tabel `testimoni`
 --
 
 INSERT INTO `testimoni` (`id_testimoni`, `id_pelanggan`, `id_so`, `testimoni_barang`) VALUES
-('T-201906022', '1126', '201906022', 'Gila sih ini gila gila'),
 ('T-201906023', '1127', '201906023', 'kita bikin startup'),
-('T-201906031', '3', '201906031', 'Keren bat para para');
+('T-201907081', '5', '201907081', 'yoman');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `customer`
+-- Indeks untuk tabel `customer`
 --
 ALTER TABLE `customer`
   ADD PRIMARY KEY (`id_pelanggan`);
 
 --
--- Indexes for table `production_order`
+-- Indeks untuk tabel `production_order`
 --
 ALTER TABLE `production_order`
   ADD PRIMARY KEY (`id_po`);
 
 --
--- Indexes for table `sales_order`
+-- Indeks untuk tabel `sales_order`
 --
 ALTER TABLE `sales_order`
   ADD PRIMARY KEY (`id_so`);
 
 --
--- Indexes for table `shopping_cart`
+-- Indeks untuk tabel `shopping_cart`
 --
 ALTER TABLE `shopping_cart`
   ADD PRIMARY KEY (`id_sc`);
 
 --
--- Indexes for table `stock_barang`
+-- Indeks untuk tabel `stock_barang`
 --
 ALTER TABLE `stock_barang`
   ADD PRIMARY KEY (`id_barang`);
 
 --
--- Indexes for table `testimoni`
+-- Indeks untuk tabel `testimoni`
 --
 ALTER TABLE `testimoni`
   ADD PRIMARY KEY (`id_testimoni`);
