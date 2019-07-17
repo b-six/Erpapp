3baa042f3f2cb4b1ffeca3c5ad7c128f4f6492b6
Mock1ng <bayuw1123@gmail.com>
2019-07-18 02:24:32 +0700

    yoman ez banget produksi UI

--- application/views/produksi/partials/css.php
+++ application/views/produksi/partials/css.php
@@ -4,6 +4,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 <!-- css     -->
-    <link href="<?php echo base_url('assets/css/materialize.min.css'); ?>" rel="stylesheet">
+    <link href="<?php echo base_url('assets/css/materialize.css'); ?>" rel="stylesheet">
     <link href="<?php echo base_url('assets/css/marketing/styles.css'); ?>" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
--- application/views/produksi/partials/navbar.php
+++ application/views/produksi/partials/navbar.php
@@ -1,22 +1,22 @@
 <div class="navbar-fixed">
     <nav class="no-shadows">
-        <div class="nav-wrapper blue-dark-grey darken-4">
+        <div class="nav-wrapper">
             <a href="dashboard" class="brand-logo"><img src="<?php echo base_url('assets/img/marketing/sosro_logo.png') ?>" alt="Sosro Logo" height="60px" width="100px"></a>
 
             <!-- Dropdown Structure -->
-            <ul id="plan_DD" class="dropdown-content indigo darken-4">
+            <ul id="plan_DD" class="dropdown-content ">
                 <li><a href="production_order" class="white-text">Production Order</a></li>
                 <li><a href="buat_planning" class="white-text">Buat Planning</a></li>
             </ul>
 
-            <ul id="produksi_DD" class="dropdown-content indigo darken-4">
+            <ul id="produksi_DD" class="dropdown-content ">
                 <li><a href="bahan_baku" class="white-text">Bahan Baku</a></li>
                 <li><a href="komposisi_produk" class="white-text">Komposisi</a></li>
                 <li><a href="data_produk_jadi" class="white-text">Data Produk Jadi</a></li>
                 <li><a href="hasil_produksi" class="white-text">Hasil Produksi</a></li>
             </ul>
 
-            <ul id="report_DD" class="dropdown-content indigo darken-4">
+            <ul id="report_DD" class="dropdown-content ">
                 <li><a href="quality_control" class="white-text">Quality Control</a></li>
                 <li><a href="laporan_perbaikan_produksi" class="white-text">Lap. Perbaikan Produksi</a></li>
                 <li><a href="laporan_produksi" class="white-text">Lap. Produksi</a></li>
@@ -25,7 +25,7 @@
 
            
 
-            <ul id="acc_DD" class="dropdown-content indigo darken-4">
+            <ul id="acc_DD" class="dropdown-content ">
                 <li class="no-pointer-event"><a href="#!" class="white-text">Manager</a></li>
                 <li class="divider"></li>
                 <li><a href="#!" class="white-text">My Account</a></li>
